cb7fe948f0ee8e63edb3b235fe155ef91629fa1c
yangming <yang756260386@gmail.com>
2017-06-21 16:06:09 +0800

    allok

--- yangtest.py
+++ yangtest.py
@@ -80,3 +80,22 @@ def getfileeverylinetolist(fname):
     # you may also want to remove whitespace characters like `\n` at the end of each line
     content = [x.strip() for x in content]
     return content
+
+
+def extractstuff(source,index):
+    targetgroup=[]
+    for k in source:
+        target=k.split()
+        yangtest.yangshow(target)
+        try:
+            targetgroup.append(target[index])
+        except:
+            pass
+    return targetgroup
+
+
+
+
+
+
+    
