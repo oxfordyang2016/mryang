871046c9eb9a6ef44a451013a211e44bd9aa0aae
yangming <yang756260386@gmail.com>
2017-06-21 13:59:42 +0800

    allok

---
+++ yangvideo.py
@@ -0,0 +1,13 @@
+import os
+def dlvideo(videolist):
+	"""
+	this functon is used to downoad video
+	"""
+    for url in videolist:
+    	print('-------------------------------------------')
+    	print('u are dealing the url=======>'+str(url))
+    	os.system('youtube-dl -f best -i '+url )
+
+
+
+    	
