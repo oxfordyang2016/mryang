d5e1f01d2da6451b6c9bab6a82145aa1119a2feb
yangming <yang756260386@gmail.com>
2017-06-23 13:43:06 +0800

    allok

--- yangtest.py
+++ yangtest.py
@@ -95,5 +95,18 @@ def extractstuff(source,index):
 
 
 
+def pinghost(hostname):
+    """
+    this function is used to stimulate the ping command
+    """
+    response = os.system("ping -c 1 " + hostname)
 
+    #anthen check the response...
+    if response == 0:
+        print(hostname, 'is up!')
+        result='ok'
+    else:
+        print(hostname, 'is down!')
+        result='no'
+    return result
 
