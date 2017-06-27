df6c6cb0bd4af001b7e59abe37a8a00074556f9e
yangming <yang756260386@gmail.com>
2017-06-27 10:49:22 +0800

    allok

---
+++ yanghelp.py
@@ -0,0 +1,31 @@
+
+__version__ = 1.0
+
+
+
+def goal():
+	'''
+	the main goal of this script is to 
+	help myself to grasp python basic 
+	core and develop the profucity
+	'''
+	print('make a good basic of python')
+
+
+
+def help():
+	helpinfo='''
+    1.get the help info ,the main function is
+
+    help(x),it will print the info included included
+
+    function head comment sector!
+	'''
+	print(helpinfo)
+
+def rename():
+    renameinfo='''
+    Use os.rename(src, dst) to rename or move a file or a directory
+    '''
+    print(renameinfo)
+
