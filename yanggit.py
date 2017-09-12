fbb7d39dfcf8cfb1ef07cc8ca262142af7c35b9e
gripleaf <425797155@qq.com>
2017-09-12 10:19:29 +0800

    allok

--- yanggit.py
+++ yanggit.py
@@ -2,7 +2,9 @@ import os
 
 def upload(comment='allok'):
 	'''
-	i dislike every time to repeat every step!!!!fuck
+	i dislike to repeat every step every time!!!!fuck
+	usage:
+	yanggit.upload('add comment of upload function')
 	'''
 	os.system('git add .')
 	os.system('git commit -m '+str(comment))
